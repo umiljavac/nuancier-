187de6df691283fe5e84a9e9ccff88e8a7e4a2e5
umiljavac <ulm_ulm@hotmail.com>
2017-01-22 14:49:00 +0100

    la largeur du nuancier et des cellules est maintenant exprimée en %, la hauteur des cellules est toujours fixe.

--- nuancier.js
+++ nuancier.js
@@ -1,6 +1,5 @@
 var Nuancier = {
-    initNuance: function (carre, rouge, vert, bleu, parent) {
-        this.carre = carre;
+    initNuance: function (rouge, vert, bleu, parent) {
         this.rouge = rouge;
         this.vert = vert;
         this.bleu = bleu;
@@ -11,27 +10,21 @@ var Nuancier = {
             var couleurAlpha = document.createElement("div");
             couleurAlpha.className = "couleur";
             couleurAlpha.style.backgroundColor = "rgba(" + this.rouge + ", " + this.vert + ", " + this.bleu + ", 0." + i + 1 + ")";
-            couleurAlpha.style.width = this.carre + "px";
-            couleurAlpha.style.height = this.carre + "px";
             divParent.appendChild(couleurAlpha);
-
-        }
+     }
         var couleur = document.createElement("div");
         couleur.style.backgroundColor = "rgba(" + this.rouge + ", " + this.vert + ", " + this.bleu + ", 1)";
-        couleur.style.width = this.carre + "px";
-        couleur.style.height = this.carre + "px";
         couleur.className = "couleur";
         divParent.appendChild(couleur);
-
     }
 };
 for (var b = 0; b <= 255; b += 51) {
     for (var g = 0; g <= 255; g += 51) {
         for (var r = 0; r <= 255; r += 51) {
             var nuancierNoir = Object.create(Nuancier);
-            nuancierNoir.initNuance(55, r, g, b, "nuancierNoir");
+            nuancierNoir.initNuance(r, g, b, "nuancierNoir");
             var nuancierBlanc = Object.create(Nuancier);
-            nuancierBlanc.initNuance(55, r, g, b, "nuancierBlanc");
+            nuancierBlanc.initNuance(r, g, b, "nuancierBlanc");
         }
     }
 }
